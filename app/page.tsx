edb34981c7bfb5fb84b76019e49d3c5f05404371
Michael Siddiqi <bismics@gmail.com>
2023-08-12 21:18:59 +0200

    Initialize project

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function HomePage() {
+  return (
+    <div>
+      <h1 className="text-4xl">Welcome to the blog</h1>
+      <p>Some content</p>
+    </div>
+  );
+}
